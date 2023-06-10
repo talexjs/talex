de0e9269103418e0cca2d2bbf339c61ebdb6ab62
fengwang <1272171893@qq.com>
2023-06-10 15:31:43 +0800

    feat: ✨ 填写cli 的内容

---
+++ packages/cli/src/index.ts
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+import { Command } from 'commander';
+import chalk from 'chalk';
+const program = new Command();
+// 配置帮助信息
+program.on('--help', () => {
+  console.log(`\r\n Run ${chalk.green(`dyi <command> --help`)} to understand the details\r\n `);
+});
+program.parse(process.argv);
